9b9ca9b91cb262eb51c13280c611eb4cf0cee1f0
Daxsum <kaldax250@gmail.com>
2022-09-15 20:45:56 +0300

    v0.3

---
+++ src/components/box.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "../style.css";
+const Box = () => {
+  return (
+    <React.Fragment>
+      <div className="scrollbar scrollbar-primary">
+        <div className="force-overflow">
+          <h3>COUNTRY NAME</h3>
+          <p className="paragraph">regin</p>
+          <p className="paragraph">city</p>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+};
+export default Box;
---
+++ src/components/map.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Box from "./box";
+const Map = () => {
+  return (
+    <div className="back">
+      <Box />
+    </div>
+  );
+};
+
+export default Map;
